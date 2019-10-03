1d44828a6d29b48e2ab37b67525f215b8fe5bcf1
Elliot Nest <elliot.nest@claromentis.com>
2019-10-03 12:07:20 +0100

    TY-12 Added optional properties 'image_url' and 'profile_url' to Thankables.

--- classes/ThankYous/Thankable.php
+++ classes/ThankYous/Thankable.php
@@ -10,15 +10,22 @@ class Thankable
 
 	private $id;
 
+	private $image_url;
+
 	private $extranet_area_id;
 
-	public function __construct(string $name, ?int $obj_type_id = null, ?int $id = null, ?int $extranet_area_id = null)
+	private $profile_url;
+
+	public function __construct(string $name, ?int $obj_type_id = null, ?int $id = null, ?int $extranet_area_id = null, ?string $image_url = null, ?string $profile_url = null)
 	{
-		$this->name = $name;
-		$this->obj_type_id = $obj_type_id;
-		$this->id = $id;
 		$this->extranet_area_id = $extranet_area_id;
+		$this->id               = $id;
+		$this->image_url        = $image_url;
+		$this->name             = $name;
+		$this->obj_type_id      = $obj_type_id;
+		$this->profile_url      = $profile_url;
 	}
+
 	/**
 	 * @return int|null
 	 */
@@ -36,6 +43,14 @@ class Thankable
 	}
 
 	/**
+	 * @return string|null
+	 */
+	public function GetImageUrl(): ?string
+	{
+		return $this->image_url;
+	}
+
+	/**
 	 * @return string
 	 */
 	public function GetName(): string
@@ -49,5 +64,13 @@ class Thankable
 	public function GetObjectTypeId(): ?int
 	{
 		return $this->obj_type_id;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function GetProfileUrl(): ?string
+	{
+		return $this->profile_url;
 	}
 }
