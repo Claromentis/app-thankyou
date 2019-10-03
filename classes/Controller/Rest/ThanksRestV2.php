17309ecabbd3dad668afcae0bfd5b6750052be1f
Elliot Nest <elliot.nest@claromentis.com>
2019-10-03 15:44:16 +0100

    TY-12 Replaced call to specific ThankYou API endpoint with generic API endpoint.

--- classes/Controller/Rest/ThanksRestV2.php
+++ classes/Controller/Rest/ThanksRestV2.php
@@ -3,7 +3,7 @@
 namespace Claromentis\ThankYou\Controller\Rest;
 
 use Claromentis\Core\Http\JsonPrettyResponse;
-use Claromentis\ThankYou\Api\ThankYous;
+use Claromentis\ThankYou\Api;
 use Claromentis\ThankYou\View\ThanksListView;
 use DateClaTimeZone;
 use Psr\Http\Message\ServerRequestInterface;
@@ -11,19 +11,19 @@ use Psr\Http\Message\ServerRequestInterface;
 class ThanksRestV2
 {
 	//TODO: Catch the exceptions
-	private $api_thank_yous;
+	private $api;
 
 	private $thank_you_view;
 
-	public function __construct(ThankYous $thank_yous, ThanksListView $thank_you_view)
+	public function __construct(Api $api, ThanksListView $thank_you_view)
 	{
-		$this->api_thank_yous = $thank_yous;
+		$this->api            = $api;
 		$this->thank_you_view = $thank_you_view;
 	}
 
 	public function GetThankYou(int $id): JsonPrettyResponse
 	{
-		$thank_you         = $this->api_thank_yous->GetThankYous($id, true);
+		$thank_you         = $this->api->ThankYous()->GetThankYous($id, true);
 		$display_thank_you = $this->thank_you_view->ConvertThankYouToArray($thank_you, DateClaTimeZone::GetCurrentTZ());
 
 		return new JsonPrettyResponse($display_thank_you);
@@ -36,7 +36,7 @@ class ThanksRestV2
 		$offset       = $query_params['offset'] ?? 0;
 		$thanked      = (bool) (int) ($query_params['thanked'] ?? null);
 
-		$thank_yous         = $this->api_thank_yous->GetRecentThankYous($limit, $offset, $thanked);
+		$thank_yous         = $this->api->ThankYous()->GetRecentThankYous($limit, $offset, $thanked);
 		$display_thank_yous = [];
 		foreach ($thank_yous as $thank_you)
 		{
