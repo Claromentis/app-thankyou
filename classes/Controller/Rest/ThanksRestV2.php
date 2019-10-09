4990f37975f676783b48c3bf8eff961da24f3812
Elliot Nest <elliot.nest@claromentis.com>
2019-10-09 09:54:13 +0100

    REST API Endpoint "/thanks/{id}" will now consider the User's Extranet when displaying the Thank You.
    TY-12 Updated ThankYous REST API Endpoint V2 to only go via the Thank Yous API rather than accessing classes directly.

--- classes/Controller/Rest/ThanksRestV2.php
+++ classes/Controller/Rest/ThanksRestV2.php
@@ -5,7 +5,6 @@ namespace Claromentis\ThankYou\Controller\Rest;
 use Claromentis\Core\Http\JsonPrettyResponse;
 use Claromentis\Core\Security\SecurityContext;
 use Claromentis\ThankYou\Api;
-use Claromentis\ThankYou\View\ThanksListView;
 use DateClaTimeZone;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -14,18 +13,16 @@ class ThanksRestV2
 	//TODO: Catch the exceptions
 	private $api;
 
-	private $thank_you_view;
-
-	public function __construct(Api $api, ThanksListView $thank_you_view)
+	public function __construct(Api $api)
 	{
-		$this->api            = $api;
-		$this->thank_you_view = $thank_you_view;
+		$this->api = $api;
 	}
 
-	public function GetThankYou(int $id): JsonPrettyResponse
+	public function GetThankYou(int $id, SecurityContext $security_context): JsonPrettyResponse
 	{
+		$extranet_area_id  = (int) $security_context->GetExtranetAreaId();
 		$thank_you         = $this->api->ThankYous()->GetThankYous($id, true);
-		$display_thank_you = $this->thank_you_view->ConvertThankYouToArray($thank_you, DateClaTimeZone::GetCurrentTZ());
+		$display_thank_you = $this->api->ThankYous()->ConvertThankYousToArrays($thank_you, DateClaTimeZone::GetCurrentTZ(), $extranet_area_id);
 
 		return new JsonPrettyResponse($display_thank_you);
 	}
@@ -33,17 +30,13 @@ class ThanksRestV2
 	public function GetThankYous(ServerRequestInterface $request, SecurityContext $security_context): JsonPrettyResponse
 	{
 		$extranet_area_id = (int) $security_context->GetExtranetAreaId();
-		$query_params = $request->getQueryParams();
-		$limit        = $query_params['limit'] ?? 20;
-		$offset       = $query_params['offset'] ?? 0;
-		$thanked      = (bool) (int) ($query_params['thanked'] ?? null);
+		$query_params     = $request->getQueryParams();
+		$limit            = $query_params['limit'] ?? 20;
+		$offset           = $query_params['offset'] ?? 0;
+		$thanked          = (bool) (int) ($query_params['thanked'] ?? null);
 
 		$thank_yous         = $this->api->ThankYous()->GetRecentThankYous($limit, $offset, $thanked, $extranet_area_id);
-		$display_thank_yous = [];
-		foreach ($thank_yous as $thank_you)
-		{
-			$display_thank_yous[] = $this->thank_you_view->ConvertThankYouToArray($thank_you, DateClaTimeZone::GetCurrentTZ(), $extranet_area_id);
-		}
+		$display_thank_yous = $this->api->ThankYous()->ConvertThankYousToArrays($thank_yous, DateClaTimeZone::GetCurrentTZ(), $extranet_area_id);
 
 		return new JsonPrettyResponse($display_thank_yous);
 	}
