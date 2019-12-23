b41d761b148dc5a0a57aa7c66beb3048cf8fca43
Elliot Nest <elliot.nest@claromentis.com>
2019-12-23 12:42:56 +0000

    TY-7 Added return Typehinting to Tag Acl.

--- classes/Tags/TagAcl.php
+++ classes/Tags/TagAcl.php
@@ -7,14 +7,28 @@ use Claromentis\Core\Security\SecurityContext;
 
 class TagAcl
 {
+	/**
+	 * @var AdminPanel $admin_panel
+	 */
 	private $admin_panel;
 
+	/**
+	 * TagAcl constructor.
+	 *
+	 * @param AdminPanel $admin_panel
+	 */
 	public function __construct(AdminPanel $admin_panel)
 	{
 		$this->admin_panel = $admin_panel;
 	}
 
-	public function CanDeleteTag(SecurityContext $context)
+	/**
+	 * Determine whether the Security Context can Delete a Tag.
+	 *
+	 * @param SecurityContext $context
+	 * @return bool
+	 */
+	public function CanDeleteTag(SecurityContext $context): bool
 	{
 		return $this->admin_panel->IsAccessible($context);
 	}
