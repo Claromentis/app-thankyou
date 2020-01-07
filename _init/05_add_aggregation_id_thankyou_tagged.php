3a85d430ff4fbc120e264bcd6e2215c3ed94c587
Elliot Nest <elliot.nest@claromentis.com>
2020-01-07 13:08:16 +0000

    TY-7 Removed Database indexing for `item_id` on tagged table, and replaced with index for item_id and aggregation_id, which is useful.

--- _init/05_add_aggregation_id_thankyou_tagged.php
+++ _init/05_add_aggregation_id_thankyou_tagged.php
@@ -4,12 +4,6 @@
  */
 $db = $migrations->GetDb();
 
-$table_description = [
-	'id'          => 'IDENTITY',
-	'item_id' => 'INT',
-	'tag_id'      => 'INT'
-];
-
 $db->AddColumn('thankyou_tagged', 'aggregation_id', 'INT');
 
 $db->CreateIndex('thankyou_tagged', 'idx_aggregation_id', 'aggregation_id');
