e62b280bfac41ae66bff3edd85c04ae359115506
Elliot Nest <elliot.nest@claromentis.com>
2020-02-13 10:36:23 +0000

    TY-7 Removed unused method.

--- classes/ThankYous/ThankYouUtility.php
+++ classes/ThankYous/ThankYouUtility.php
@@ -49,66 +49,6 @@ class ThankYouUtility
 	}
 
 	/**
-	 * @param DateTime[] $date_range
-	 * @return string[]
-	 */
-	public function FormatDateRange(array $date_range): array
-	{
-		if (!isset($date_range[0]) || !($date_range[0] instanceof DateTime))
-		{
-			throw new InvalidArgumentException("Failed to Format Date Range, Argument 1 offset 0 must be a DateTime");
-		}
-		if (!isset($date_range[1]) || !($date_range[1] instanceof DateTime))
-		{
-			throw new InvalidArgumentException("Failed to Format Date Range, Argument 1 offset 1 must be a DateTime");
-		}
-
-		/**
-		 * @var DateTime $from_date
-		 */
-		$from_date = clone $date_range[0];
-		$from_date->setTimezone(new DateTimeZone('UTC'));
-
-		$date_range[0] = $from_date->format('YmdHis');
-
-		/**
-		 * @var DateTime $from_date
-		 */
-		$to_date = clone $date_range[1];
-		$to_date->setTimezone(new DateTimeZone('UTC'));
-
-		$date_range[1] = $to_date->format('YmdHis');
-
-		return $date_range;
-	}
-
-	/**
-	 * @param array $orders
-	 * @return string
-	 */
-	public function BuildOrderString(array $orders): string
-	{
-		if (count($orders) === 0)
-		{
-			return '';
-		}
-
-		$query_string = " ORDER BY";
-		foreach ($orders as $offset => $order)
-		{
-			$column    = $order['column'] ?? null;
-			$direction = (isset($order['desc']) && $order['desc'] === true) ? 'DESC' : 'ASC';
-			if (!isset($column) || !is_string($column))
-			{
-				throw new InvalidArgumentException("Failed to GetTags, one or more Orders does not have a column");
-			}
-			$query_string .= " " . $column . " " . $direction;
-		}
-
-		return $query_string;
-	}
-
-	/**
 	 * Create a Thank You's URL (not including the site's name)
 	 *
 	 * @param int $id
